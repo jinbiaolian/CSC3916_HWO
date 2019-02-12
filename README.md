f2f02a0565e3216f4a87f279e2a49be5c82cc679
jinbiaolian <36168804+jinbiaolian@users.noreply.github.com>
2019-02-12 00:17:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# week1
+# CSC3916_HW0
+# Jinbiao Lian
+[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/1b39b4197d70391d4ab4#?env%5Bbook_title%5D=W3sia2V5IjoiYm9va190aXRsZSIsInZhbHVlIjoiVHVyaW5nIiwiZGVzY3JpcHRpb24iOiIiLCJ0eXBlIjoidGV4dCIsImVuYWJsZWQiOnRydWV9LHsia2V5IjoiaWQiLCJ2YWx1ZSI6IiIsImRlc2NyaXB0aW9uIjoiIiwidHlwZSI6InRleHQiLCJlbmFibGVkIjp0cnVlfV0=)
---
+++ utils/googlebooks.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+
+module.exports = async (phrase) => {
+    const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+        params: {
+            format: 'json',
+            q: `${phrase}`
+        }
+    })
+    var response = {
+        data: results.data,
+        status: results.status,
+        statusText: results.statusText,
+        headers: results.headers,
+        requestHeader: results.config.headers
+    }
+    return JSON.stringify(response.data);
+}
